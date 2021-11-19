feb2fb09d409a01a9fb3991e253af78966c22b0f
ensarkovankaya <ensar@kovankaya.com>
2021-11-20 02:50:45 +0300

    add admin views for Cuisine, Dinner and Restaurant models

--- api/delivery/admin.py
+++ api/delivery/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from delivery.models import Cuisine, Dinner, Restaurant
+
+
+class CuisineAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+    list_display = ('name', 'created_at', 'modified_at')
+
+
+class DinnerAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+    list_filter = ('category', )
+    list_display = ('name', 'category', 'created_at', 'modified_at')
+
+
+class RestaurantAdmin(admin.ModelAdmin):
+    search_fields = ('name', )
+    list_display = ('name', 'created_at', 'modified_at')
+    filter_horizontal = ('menu',)
+
+
+admin.site.register(Cuisine, CuisineAdmin)
+admin.site.register(Dinner, DinnerAdmin)
+admin.site.register(Restaurant, RestaurantAdmin)
