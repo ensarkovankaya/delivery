575755d37d512d282dc59ee15a127a911999907c
ensarkovankaya <ensar@kovankaya.com>
2021-11-20 01:02:51 +0300

    add first models

---
+++ api/yemeksepeti/migrations/0001_initial.py
@@ -0,0 +1,47 @@
+import django.core.validators
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+    initial = True
+
+    dependencies = []
+
+    operations = [
+        migrations.CreateModel(
+            name='Cuisine',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=255)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('modified_at', models.DateTimeField(auto_now=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Dinner',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=255)),
+                ('price', models.DecimalField(
+                    decimal_places=2,
+                    max_digits=5,
+                    validators=[django.core.validators.MinValueValidator(0)],
+                )),
+                ('currency', models.CharField(choices=[('TRY', 'TRY'), ('USD', 'USD'), ('EUR', 'EUR')], max_length=3)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('modified_at', models.DateTimeField(auto_now=True)),
+                ('category', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='yemeksepeti.cuisine')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Restaurant',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=255)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('modified_at', models.DateTimeField(auto_now=True)),
+                ('menu', models.ManyToManyField(to='yemeksepeti.Dinner')),
+            ],
+        ),
+    ]
--- api/yemeksepeti/models.py
+++ api/yemeksepeti/models.py
@@ -1,3 +1,28 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Cuisine(models.Model):
+    name = models.CharField(max_length=255)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+
+class Dinner(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(to=Cuisine, on_delete=models.CASCADE)
+
+    price = models.DecimalField(max_digits=5, decimal_places=2, validators=[MinValueValidator(0)])
+    currency = models.CharField(max_length=3, choices=[('TRY', 'TRY'), ('USD', 'USD'), ('EUR', 'EUR')])
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255)
+    menu = models.ManyToManyField(to=Dinner)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
