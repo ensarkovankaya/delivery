cb1be179915d4a2f3ca04ca6ec7d697d406ce942
ensarkovankaya <ensar@kovankaya.com>
2021-11-22 23:02:34 +0300

    add channel enum

--- api/utils/redis.py
+++ api/utils/redis.py
@@ -1,3 +1,4 @@
+import enum
 import time
 from typing import Callable
 
@@ -7,6 +8,12 @@ from django.conf import settings
 from utils.singleton import Singleton
 
 
+class Channel(enum.Enum):
+    ORDER = 'order'
+    ORDER_CREATED = ORDER + '.created'
+    ORDER_UPDATED = ORDER + '.updated'
+
+
 class PubSubClient(metaclass=Singleton):
     INTERVAL = 1000
 
@@ -14,11 +21,11 @@ class PubSubClient(metaclass=Singleton):
         self.redis_client = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT)
         self.pub_sub_client = self.redis_client.pubsub()
 
-    def register(self, channel: str, func: Callable):
-        self.pub_sub_client.subscribe(**{channel: func})
+    def register(self, channel: Channel, func: Callable):
+        self.pub_sub_client.subscribe(**{channel.value: func})
 
-    def publish(self, channel: str, message):
-        self.redis_client.publish(channel=channel, message=message)
+    def publish(self, channel: Channel, message):
+        self.redis_client.publish(channel=channel.value, message=message)
 
     def listen(self):
         while True:
