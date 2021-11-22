f4abdb4d4731384c880c06dfaf84e2d4f5dd5694
ensarkovankaya <ensar@kovankaya.com>
2021-11-22 22:56:56 +0300

    make redis client singleton

--- api/utils/redis.py
+++ api/utils/redis.py
@@ -1,7 +1,29 @@
-import redis
+import time
+from typing import Callable
 
+import redis
 from django.conf import settings
 
+from utils.singleton import Singleton
 
-def get_client() -> redis.Redis:
-    return redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT)
+
+class PubSubClient(metaclass=Singleton):
+    INTERVAL = 1000
+
+    def __init__(self):
+        self.redis_client = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT)
+        self.pub_sub_client = self.redis_client.pubsub()
+
+    def register(self, channel: str, func: Callable):
+        self.pub_sub_client.subscribe(**{channel: func})
+
+    def publish(self, channel: str, message):
+        self.redis_client.publish(channel=channel, message=message)
+
+    def listen(self):
+        while True:
+            self.pub_sub_client.get_message()
+            time.sleep(self.INTERVAL)
+
+
+pub_sub = PubSubClient()
