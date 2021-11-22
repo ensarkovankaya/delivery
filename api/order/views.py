11de96289dfb0e6d94279452f65221a03f96d80b
ensarkovankaya <ensar@kovankaya.com>
2021-11-22 20:27:36 +0300

    add authentication permission

--- api/order/views.py
+++ api/order/views.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from order.models import Order
@@ -5,5 +6,8 @@ from order.serializers import OrderSerializer
 
 
 class OrderViewSet(ModelViewSet):
-    queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
